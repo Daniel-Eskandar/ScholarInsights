e0f3d7e9b4c16c3f8c3fd75a0e07fcf45151e3cb
SuperHecht <simonhanrath20@gmail.com>
2023-12-02 15:36:50 +0100

    added data scraper and scraped some profs

---
+++ Data/serpAPI_scraper.py
@@ -0,0 +1,106 @@
+
+
+import os
+from serpapi import GoogleSearch
+import time
+import csv
+from dotenv import load_dotenv
+load_dotenv()
+
+
+#Order
+# Geiger, Hennig, von Luxburg, Butz
+# Hein, Lensch, Macke, Pons-Moll
+# Zell, Schilling, Wichmann, Williamson
+# Schölkopf
+AUTHOR_ID_LIST=['SrVnrPcAAAAJ', 'UeG5w08AAAAJ', 'mMifMdoAAAAJ', 'dIcpfzAAAAAJ',
+                '0ZAb3tsAAAAJ', '2R22h84AAAAJ', 'FKOqtF8AAAAJ', 'OpXMNnMAAAAJ',
+                'u7w1MYcAAAAJ', '0uEkSPQAAAAJ', 'NxrQ794AAAAJ', 'G4MBruQAAAAJ',
+                'DZ-fHPgAAAAJ' ]
+
+
+
+# Saves the scraped articles of a prof as csv
+def saveAsCSV(cur_author, articles, location=None):
+
+    #remove dots and spaces from main author_name
+    name_cleaned = cur_author.translate(str.maketrans('', '', ' .'))
+    if location == None:
+        location = f'Data/articles_{name_cleaned}.csv'
+    
+
+    with open(location, mode='w', newline='', encoding='utf-8') as csv_file:
+        # Add 'author_name' at the beginning of fieldnames and exclude 'link' and 'citation_id'
+        fieldnames = ['author_name'] + [key for key in articles[0].keys() if key not in ['link', 'citation_id']]
+        
+        # Replace 'cited_by' in fieldnames with 'cited_by_value'
+        fieldnames = ['cited_by_value' if field == 'cited_by' else field for field in fieldnames]
+
+        writer = csv.DictWriter(csv_file, fieldnames=fieldnames)
+
+        writer.writeheader()
+
+        # Write the rows
+        for article in articles:
+            row = {'author_name': cur_author}
+
+            # Exclude 'link' and 'citation_id' fields and extract 'value' from 'cited_by'
+            for key in fieldnames:
+                if key in article and key not in ['author_name', 'cited_by_value', 'link', 'citation_id']:
+                    row[key] = article[key]
+                if key == 'cited_by_value' and 'cited_by' in article:
+                    row[key] = article['cited_by'].get('value', '')
+
+            writer.writerow(row)
+
+
+
+def fetch_results(params):
+    print('token used')
+    search = GoogleSearch(params)
+    results = search.get_dict()
+    return results
+
+
+def main():
+
+    api_key = os.getenv('SERPAPI_KEY')
+
+    for author_id in AUTHOR_ID_LIST:
+
+        print(author_id)
+
+        params = {
+            "engine": "google_scholar_author",
+            "author_id": author_id,
+            "api_key": api_key,
+            "start": 0
+        }
+
+        articles = []
+
+        # Fetch the first set of results
+        results = fetch_results(params)
+        cur_author = results['author']['name']
+        articles.extend(results.get("articles", []))
+
+        # Continue fetching until 'next' is not present
+        while 'next' in results.get("serpapi_pagination", {}):
+            params["start"] += 20  # Assuming 20 results per page
+            results = fetch_results(params)
+            articles.extend(results.get("articles", []))
+            time.sleep(1)  # Pause to avoid rate limiting
+
+        saveAsCSV(cur_author, articles)
+
+
+
+
+
+if __name__ == "__main__":
+    main()
+
+
+
+
+
---
+++ read_csv.py
@@ -0,0 +1,26 @@
+import pandas as pd
+from collections import Counter
+
+
+# Path to your CSV file
+file_path = 'Data/articles_PhilippHennig.csv'
+
+# Read the CSV into a DataFrame
+df = pd.read_csv(file_path)
+
+# Display the first few rows of the DataFrame
+print(df.head())
+
+all_authors = list(df['authors'])
+all_authors_flattened = flattened_list = [name.strip() for item in all_authors for name in item.split(',')]
+
+
+name_counts = Counter(all_authors_flattened)
+
+# Sort the dictionary by value in descending order
+sorted_name_counts = sorted(name_counts.items(), key=lambda item: item[1], reverse=True)
+
+# Convert sorted tuples back to dictionary
+sorted_name_counts_dict = dict(sorted_name_counts)
+
+print(sorted_name_counts_dict)
