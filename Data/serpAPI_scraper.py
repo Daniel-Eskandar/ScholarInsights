f8c7cd7a7645dc45d2509c39c0b06232ee447239
Daniel Eskandar <daniel.b.eskandar@gmail.com>
2023-12-06 13:26:52 +0100

    add rest of profs and applied pub date threshold

--- Data/serpAPI_scraper.py
+++ Data/serpAPI_scraper.py
@@ -1,36 +1,34 @@
-
-
 import os
 from serpapi import GoogleSearch
 import time
 import csv
 from dotenv import load_dotenv
-load_dotenv()
 
+load_dotenv()
 
 #Order
 # Geiger, Hennig, von Luxburg, Butz
 # Hein, Lensch, Macke, Pons-Moll
 # Zell, Schilling, Wichmann, Williamson
 # Schölkopf
-AUTHOR_ID_LIST=['u7w1MYcAAAAJ', '0uEkSPQAAAAJ', 'NxrQ794AAAAJ', 'G4MBruQAAAAJ',
-                'DZ-fHPgAAAAJ' ]
+AUTHOR_ID_LIST = ['u7w1MYcAAAAJ', '0uEkSPQAAAAJ', 'NxrQ794AAAAJ', 'G4MBruQAAAAJ', 'DZ-fHPgAAAAJ']
 
+# Order: Black, Kuchenbecker, Akata, Martius, Krone, Pfeifer, Bringmann, Maghsudi, Ochs, Kasneci, Berens, Eickhoff, Bethge
+AUTHOR_ID_LIST_2 = ['b-JF-UIAAAAJ', 'srDA4dAAAAAJ', 'vXQwphYAAAAJ&hl', 'pk53ZkAAAAAJ', 'lgiJRgoAAAAJ', 'Llw9zY4AAAAJ', 'bZVkVvoAAAAJ', 'lPQLk3QAAAAJ', 'QQi1_rAAAAAJ', '0z0fNxUAAAAJ']
 
+threshold_year = 2015
 
 # Saves the scraped articles of a prof as csv
 def saveAsCSV(cur_author, articles, location=None):
-
-    #remove dots and spaces from main author_name
+    # remove dots and spaces from main author_name
     name_cleaned = cur_author.translate(str.maketrans('', '', ' .'))
     if location == None:
-        location = f'Data/articles_{name_cleaned}.csv'
-    
+        location = f'./articles_{name_cleaned}.csv'
 
     with open(location, mode='w', newline='', encoding='utf-8') as csv_file:
         # Add 'author_name' at the beginning of fieldnames and exclude 'link' and 'citation_id'
         fieldnames = ['author_name'] + [key for key in articles[0].keys() if key not in ['link', 'citation_id']]
-        
+
         # Replace 'cited_by' in fieldnames with 'cited_by_value'
         fieldnames = ['cited_by_value' if field == 'cited_by' else field for field in fieldnames]
 
@@ -40,17 +38,17 @@ def saveAsCSV(cur_author, articles, location=None):
 
         # Write the rows
         for article in articles:
-            row = {'author_name': cur_author}
+            if article["year"] != "" and int(article["year"]) >= threshold_year:
+                row = {'author_name': cur_author}
 
-            # Exclude 'link' and 'citation_id' fields and extract 'value' from 'cited_by'
-            for key in fieldnames:
-                if key in article and key not in ['author_name', 'cited_by_value', 'link', 'citation_id']:
-                    row[key] = article[key]
-                if key == 'cited_by_value' and 'cited_by' in article:
-                    row[key] = article['cited_by'].get('value', '')
+                # Exclude 'link' and 'citation_id' fields and extract 'value' from 'cited_by'
+                for key in fieldnames:
+                    if key in article and key not in ['author_name', 'cited_by_value', 'link', 'citation_id']:
+                        row[key] = article[key]
+                    if key == 'cited_by_value' and 'cited_by' in article:
+                        row[key] = article['cited_by'].get('value', '')
 
-            writer.writerow(row)
-
+                writer.writerow(row)
 
 
 def fetch_results(params):
@@ -61,17 +59,16 @@ def fetch_results(params):
 
 
 def main():
-
     api_key = os.getenv('SERPAPI_KEY')
 
     for author_id in AUTHOR_ID_LIST:
-
         print(author_id)
 
         params = {
             "engine": "google_scholar_author",
             "author_id": author_id,
             "api_key": api_key,
+            "sort": "pubdate",
             "start": 0
         }
 
@@ -83,7 +80,7 @@ def main():
         articles.extend(results.get("articles", []))
 
         # Continue fetching until 'next' is not present
-        while 'next' in results.get("serpapi_pagination", {}):
+        while 'next' in results.get("serpapi_pagination", {}) and articles[-1]["year"] != "" and int(articles[-1]["year"]) >= threshold_year:
             params["start"] += 20  # Assuming 20 results per page
             results = fetch_results(params)
             articles.extend(results.get("articles", []))
@@ -92,13 +89,5 @@ def main():
         saveAsCSV(cur_author, articles)
 
 
-
-
-
 if __name__ == "__main__":
     main()
-
-
-
-
-
