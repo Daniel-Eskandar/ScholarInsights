b0298314a6816f3a82d81e8a687fcdc89f472c86
Daniel Eskandar <daniel.b.eskandar@gmail.com>
2023-12-13 18:23:15 +0100

    Replaced bib files with xml files to correct publication type

--- dblp/create_dataset.py
+++ dblp/create_dataset.py
@@ -1,53 +1,86 @@
 import os
 import csv
-import bibtexparser
+import xml.etree.ElementTree as ET
+import re
 
 
-def process_bib_file(file_path):
-    professor_name = os.path.splitext(os.path.basename(file_path))[0]
-    with open(file_path, 'r', encoding='utf-8') as bib_file:
-        bib_database = bibtexparser.load(bib_file)
+def get_publication_type(pub_element):
+    if 'publtype' in pub_element.attrib:
+        if pub_element.attrib['publtype'].lower() == 'informal':
+            return 'Informal or Other Publication'
+        else:
+            return pub_element.attrib['publtype']
+    else:
+        if pub_element.find('journal') is not None:
+            return 'Journal Article'
+        elif pub_element.find('booktitle') is not None:
+            return 'Conference or Workshop Paper'
+        else:
+            return 'Informal or Other Publication'
 
-    rows = []
-    for entry in bib_database.entries:
-        if entry['ENTRYTYPE'] == 'inproceedings':
-            authors = entry.get('author', None).replace(' and\n', ';')
-            venue = entry.get('booktitle', None)
-            title = entry.get('title', None)
-            year = entry.get('year', None)
-            row = [professor_name, 'Journal', venue, title, authors, year]
-            rows.append(row)
-        elif entry['ENTRYTYPE'] == 'article':
-            authors = entry.get('author', None).replace(' and\n', ';')
-            venue = entry.get('journal', None)
-            title = entry.get('title', None)
-            year = entry.get('year', None)
-            row = [professor_name, 'Paper', venue, title, authors.replace(' and ', ';'), year]
-            rows.append(row)
-    return rows
+
+def extract_data_from_xml(xml_file):
+    tree = ET.parse(xml_file)
+    root = tree.getroot()
+
+    professor_name = root.attrib['name']
+    professor_name = re.sub(r'\d+$', '', professor_name).strip()
+
+    publications = []
+
+    for article in root.findall('.//r/*'):
+        if article.tag == 'article' or article.tag == 'inproceedings':
+            publication_type = get_publication_type(article)
+            venue = article.find('journal')
+            if venue is None:
+                venue = article.find('booktitle')
+            if venue is None:
+                venue = ''
+            else:
+                venue = venue.text
+
+            title = article.find('title').text if article.find('title') is not None else ''
+            authors = [re.sub(r'\d+$', '', author.text).strip() for author in article.findall('author')]
+            authors = ';'.join(authors)
+            year = article.find('year').text if article.find('year') is not None else ''
+
+            publications.append({
+                'Professor': professor_name,
+                'Publication Type': publication_type,
+                'Venue': venue,
+                'Title': title,
+                'Authors': authors,
+                'Year': year
+            })
+
+    return publications, professor_name
+
+
+def save_to_csv(data, filename):
+    keys = data[0].keys() if data else []
+    with open(filename, 'w', newline='', encoding='utf-8') as csvfile:
+        writer = csv.DictWriter(csvfile, fieldnames=keys)
+        writer.writeheader()
+        writer.writerows(data)
+
+
+def create_dataset_from_xml_folder(xml_folder, output_filename):
+    all_publications = []
+
+    for xml_file in os.listdir(xml_folder):
+        if xml_file.endswith('.xml'):
+            xml_path = os.path.join(xml_folder, xml_file)
+            publications, professor_name = extract_data_from_xml(xml_path)
+            all_publications.extend(publications)
+            print(f"Finished processing '{professor_name}' publications")
+
+    save_to_csv(all_publications, output_filename)
 
 
 def main():
-    bib_folder = 'bib'
-
-    all_rows = []
-    for file_name in os.listdir(bib_folder):
-        if file_name.endswith('.bib'):
-            file_path = os.path.join(bib_folder, file_name)
-            professor_rows = process_bib_file(file_path)
-            all_rows.extend(professor_rows)
-
-            print(f"{file_name} finished")
-
-    csv_columns = ["Professor", "Publication Type", "Venue", "Title", "Authors", "Year"]
-    csv_file = 'dataset.csv'
-
-    with open(csv_file, 'w', newline='', encoding='utf-8') as csvfile:
-        writer = csv.writer(csvfile)
-        writer.writerow(csv_columns)
-        writer.writerows(all_rows)
-
-    print(f"{csv_file} created")
+    xml_folder_path = 'xml'
+    output_csv_filename = 'dataset.csv'
+    create_dataset_from_xml_folder(xml_folder_path, output_csv_filename)
 
 
 if __name__ == "__main__":
